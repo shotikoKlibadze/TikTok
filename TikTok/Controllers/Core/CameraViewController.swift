c46a38e7903aeddf3f954ea7d2bfa43360fc69be
shotikoKlibadze <sh.klibadze@gmail.com>
2022-05-27 22:39:54 +0400

    added camera viewController functionality

--- TikTok/Controllers/Core/CameraViewController.swift
+++ TikTok/Controllers/Core/CameraViewController.swift
@@ -6,17 +6,102 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
+    
+    //Capture session
+    var captureSession = AVCaptureSession()
+    
+    //Capture Device
+    var videoCaptureDevice: AVCaptureDevice?
+    
+    //Capture Output
+    var captureOutput = AVCaptureMovieFileOutput()
+    
+    //Capture Preview
+    var capturePreviewLayer: AVCaptureVideoPreviewLayer?
+    
+    private let cameraView : UIView = {
+        let view = UIView()
+        view.clipsToBounds = true
+        view.backgroundColor = .black
+        return view
+    }()
+    
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(cameraView)
         view.backgroundColor = .systemBackground
-        // Do any additional setup after loading the view.
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .close, target: self, action: #selector(didTapClose))
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setUpCamera()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        cameraView.frame = view.bounds
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        tabBarController?.tabBar.isHidden = true
+       
+    }
+    
+    @objc func didTapClose() {
+        captureSession.stopRunning()
+        tabBarController?.tabBar.isHidden = false
+        tabBarController?.selectedIndex = 0
+    }
+    
+    func setUpCamera() {
+        //add devices
+        if let audioDevice = AVCaptureDevice.default(for: .audio) {
+            if let audioInput = try? AVCaptureDeviceInput(device: audioDevice) {
+                if captureSession.canAddInput(audioInput) {
+                    captureSession.addInput(audioInput)
+                }
+            }
+        }
+        if let videoDevice = AVCaptureDevice.default(for: .video) {
+            if let videoInput = try? AVCaptureDeviceInput(device: videoDevice) {
+                if captureSession.canAddInput(videoInput) {
+                    captureSession.addInput(videoInput)
+                }
+            }
+        }
+        // update session
+        captureSession.sessionPreset = .hd1280x720
+        if captureSession.canAddOutput(captureOutput) {
+            captureSession.addOutput(captureOutput)
+        }
+        //configure preview
+        capturePreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        capturePreviewLayer?.videoGravity = .resizeAspectFill
+        capturePreviewLayer?.frame = view.bounds
+        if let layer = capturePreviewLayer {
+            cameraView.layer.addSublayer(layer)
+        }
+        
+        //enable camera start
+        captureSession.startRunning()
+    }
 
   
  
 
+}
+
+extension CameraViewController : AVCaptureFileOutputRecordingDelegate {
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+        guard error == nil else { return }
+        
+        print("FiniShed recording to url : \(outputFileURL.absoluteString)" )
+    }
 }
--- TikTok/Controllers/Core/TabBarViewController.swift
+++ TikTok/Controllers/Core/TabBarViewController.swift
@@ -50,12 +50,19 @@ class TabBarViewController: UITabBarController {
         
         let nav1 = UINavigationController(rootViewController: home)
         let nav2 = UINavigationController(rootViewController: explore)
+        let cameraNav = UINavigationController(rootViewController: camera)
         let nav3 = UINavigationController(rootViewController: notifiactions)
         let nav4 = UINavigationController(rootViewController: profile)
+        
+       // cameraNav.navigationBar.isHidden = true
         
         nav1.navigationBar.backgroundColor = .clear
         nav1.navigationBar.setBackgroundImage(UIImage(), for: .default)
         nav1.navigationBar.shadowImage = UIImage()
+        
+        cameraNav.navigationBar.backgroundColor = .clear
+        cameraNav.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        cameraNav.navigationBar.shadowImage = UIImage()
         
         nav1.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), tag: 1)
         nav2.tabBarItem = UITabBarItem(title: "Explore", image: UIImage(systemName: "safari"), tag: 2)
@@ -63,7 +70,7 @@ class TabBarViewController: UITabBarController {
         nav3.tabBarItem = UITabBarItem(title: "Notifiactions", image: UIImage(systemName: "bell"), tag: 4)
         nav4.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName: "person.circle"), tag: 5)
         
-        setViewControllers([nav1, nav2, camera, nav3, nav4], animated: false)
+        setViewControllers([nav1, nav2, cameraNav, nav3, nav4], animated: false)
     }
     
 }
